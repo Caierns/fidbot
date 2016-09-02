dc03ac615a0c62bb1d27dd5d3857d867319e6b2e
Fiddlekins <fiddlekins@gmail.com>
2016-09-02 22:40:43 +0100

    Added rudimentary dice configuration

--- fidbot.js
+++ fidbot.js
@@ -12,6 +12,20 @@ fidbot.on('message', function(message){
 	var messageContent = message.content;
 	if (messageContent.slice(0, 5) === '/dice') {
 		fidbot.sendMessage(message.channel, dice.eval(messageContent.slice(6)));
+	} else if (messageContent.slice(0, 14) === '/configureDice') {
+		var server = message.server;
+		var roles = server.roles;
+		var roleDiceMaster = roles.get('name', 'DiceMaster');
+
+		if (roleDiceMaster && message.author.hasRole(roleDiceMaster)) {
+			var config = {
+				maxCount: parseInt(messageContent.slice(15), 10)
+			};
+			dice = new Dice(config);
+			fidbot.reply(message, 'Dice reconfigured!');
+		} else {
+			fidbot.reply(message, "I'm afraid I can't let you do that.");
+		}
 	} else if (/alerni/i.test(messageContent) && /slut/i.test(messageContent)) {
 		fidbot.sendMessage(message.channel, 'A slut! A SLUUUUUUTTTTT!');
 	}
