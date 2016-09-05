889f6472a5bd1b85d4d6bad0aa8fde9aac0ddebb
Fiddlekins <fiddlekins@gmail.com>
2016-09-05 22:07:47 +0100

    Refactored NumberRange in es6 notation

--- js/dice/numberRange.js
+++ js/dice/numberRange.js
@@ -1,43 +1,45 @@
 'use strict';
 
-var NumberRange = function(){
-	this._greaterThan = Infinity;
-	this._lessThan = -Infinity;
-	this._specificEquality = {};
-};
-
-NumberRange.prototype.addSpecificEquality = function(value){
-	this._specificEquality[value] = true;
-};
-
-NumberRange.prototype.removeSpecificEquality = function(value){
-	this._specificEquality[value] = false;
-};
-
-NumberRange.prototype.addGreaterThan = function(value){
-	this._greaterThan = Math.min(value, this._greaterThan);
-};
-
-NumberRange.prototype.setGreaterThan = function(value){
-	this._greaterThan = value;
-};
-
-NumberRange.prototype.addLessThan = function(value){
-	this._lessThan = Math.max(value, this._lessThan);
-};
-
-NumberRange.prototype.setLessThan = function(value){
-	this._lessThan = value;
-};
-
-NumberRange.prototype.isInRange = function(number){
-	if (number >= this._greaterThan) {
-		return true;
+class NumberRange {
+	constructor(){
+		this._greaterThan = Infinity;
+		this._lessThan = -Infinity;
+		this._specificEquality = {};
 	}
-	if (number <= this._lessThan) {
-		return true;
+
+	addSpecificEquality(value){
+		this._specificEquality[value] = true;
 	}
-	return !!this._specificEquality[number];
-};
+
+	removeSpecificEquality(value){
+		this._specificEquality[value] = false;
+	}
+
+	addGreaterThan(value){
+		this._greaterThan = Math.min(value, this._greaterThan);
+	}
+
+	setGreaterThan(value){
+		this._greaterThan = value;
+	}
+
+	addLessThan(value){
+		this._lessThan = Math.max(value, this._lessThan);
+	}
+
+	setLessThan(value){
+		this._lessThan = value;
+	}
+
+	isInRange(number){
+		if (number >= this._greaterThan) {
+			return true;
+		}
+		if (number <= this._lessThan) {
+			return true;
+		}
+		return !!this._specificEquality[number];
+	}
+}
 
 module.exports = NumberRange;
