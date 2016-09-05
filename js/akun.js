a45a67b3b6c05f206412c9fb0e4b6d0821bd74d2
Fiddlekins <fiddlekins@gmail.com>
2016-09-05 17:42:24 +0100

    Fixed missing live akun quests

--- js/akun.js
+++ js/akun.js
@@ -10,7 +10,10 @@ Akun.prototype.eval = function(inputString, callback){
 	if (inputString === 'live' || inputString === 'livelink') {
 		http.request({
 			host: 'anonkun.com',
-			path: '/api/anonkun/board/live'
+			path: '/api/anonkun/board/live',
+			headers: {
+				'Cookie': '__cfduid=d95fdab71be8cd3017fe3d6796bfed6931473093587; ajs_group_id=null; ajs_anonymous_id=%222cced225-0eea-4026-b2ad-d6ff046b4d51%22; loginToken=%7B%22loginToken%22%3A%22qTANYN89zyFKvyEQf%22%2C%22userId%22%3A%22C8x2fwWvtRvr4CyFm%22%7D; ajs_user_id=%22C8x2fwWvtRvr4CyFm%22'
+			}
 		}, function(response){
 			var str = '';
 
